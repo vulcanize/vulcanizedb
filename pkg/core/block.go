db65d544fb73a9bca195d87c916febac177e8f07
Matt Krump <mkrump@8thlight.com>
2018-03-06 08:37:29 -0600

    Upgrade go-ethereum to v1.8

--- pkg/core/block.go
+++ pkg/core/block.go
@@ -4,15 +4,15 @@ type Block struct {
 	Reward       float64 `db:"reward"`
 	Difficulty   int64   `db:"difficulty"`
 	ExtraData    string  `db:"extra_data"`
-	GasLimit     int64   `db:"gaslimit"`
-	GasUsed      int64   `db:"gasused"`
+	GasLimit     uint64  `db:"gaslimit"`
+	GasUsed      uint64  `db:"gasused"`
 	Hash         string  `db:"hash"`
 	IsFinal      bool    `db:"is_final"`
 	Miner        string  `db:"miner"`
 	Nonce        string  `db:"nonce"`
 	Number       int64   `db:"number"`
 	ParentHash   string  `db:"parenthash"`
-	Size         int64   `db:"size"`
+	Size         string  `db:"size"`
 	Time         int64   `db:"time"`
 	Transactions []Transaction
 	UncleHash    string  `db:"uncle_hash"`
