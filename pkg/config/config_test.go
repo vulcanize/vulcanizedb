5f8a69db24b4ce61009ecf52a6e8f8c025d37066
Matt Krump <mkrump@8thlight.com>
2018-03-22 09:13:57 -0500

    Remove test and travis deps on private dev network

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,24 +1,38 @@
 package config_test
 
 import (
+	"bytes"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/spf13/viper"
 )
 
+var vulcanizeConfig = []byte(`
+[database]
+name = "dbname"
+hostname = "localhost"
+port = 5432
+
+[client]
+ipcPath = "IPCPATH/geth.ipc"
+`)
+
 var _ = Describe("Loading the config", func() {
 
 	It("reads the private config using the environment", func() {
-		testConfig := viper.New()
-		testConfig.SetConfigName("private")
-		testConfig.AddConfigPath("$GOPATH/src/github.com/vulcanize/vulcanizedb/environments/")
-		err := testConfig.ReadInConfig()
+		viper.SetConfigName("config")
+		viper.AddConfigPath("$GOPATH/src/github.com/vulcanize/vulcanizedb/environments/")
 		Expect(viper.Get("client.ipcpath")).To(BeNil())
+
+		testConfig := viper.New()
+		testConfig.SetConfigType("toml")
+		err := testConfig.ReadConfig(bytes.NewBuffer(vulcanizeConfig))
 		Expect(err).To(BeNil())
 		Expect(testConfig.Get("database.hostname")).To(Equal("localhost"))
-		Expect(testConfig.Get("database.name")).To(Equal("vulcanize_private"))
+		Expect(testConfig.Get("database.name")).To(Equal("dbname"))
 		Expect(testConfig.Get("database.port")).To(Equal(int64(5432)))
-		Expect(testConfig.Get("client.ipcpath")).To(Equal("test_data_dir/geth.ipc"))
+		Expect(testConfig.Get("client.ipcpath")).To(Equal("IPCPATH/geth.ipc"))
 	})
 
 })
