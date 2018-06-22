7b507667f8eff76f2ad83450d9baa58af18d082c
Maxim Krasilnikov <mak.krasilnikov@gmail.com>
2018-06-22 18:28:34 +0300

    Added user and password to db config

--- pkg/config/database.go
+++ pkg/config/database.go
@@ -5,9 +5,15 @@ import "fmt"
 type Database struct {
 	Hostname string
 	Name     string
+	User     string
+	Password string
 	Port     int
 }
 
 func DbConnectionString(dbConfig Database) string {
+	if len(dbConfig.User) > 0 && len(dbConfig.Password) > 0 {
+		return fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable",
+			dbConfig.User, dbConfig.Password, dbConfig.Hostname, dbConfig.Port, dbConfig.Name)
+	}
 	return fmt.Sprintf("postgresql://%s:%d/%s?sslmode=disable", dbConfig.Hostname, dbConfig.Port, dbConfig.Name)
 }
