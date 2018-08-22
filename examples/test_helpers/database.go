17fdc8c5a2dee91f827e60dbf4929754aebf2ced
Ian Norden <iannordenn@gmail.com>
2018-08-22 13:10:23 -0500

    fixed tests for getting balanceOf and allowance, added balanceOf and allowance to the repo and repo tests and updated sql schema with new token_balance and token_allowance tables to accept this data

--- examples/test_helpers/database.go
+++ examples/test_helpers/database.go
@@ -30,6 +30,23 @@ type TokenSupplyDBRow struct {
 	TokenAddress string `db:"token_address"`
 }
 
+type TokenBalanceDBRow struct {
+	ID                 int64  `db:"id"`
+	Balance            int64  `db:"balance"`
+	BlockID            int64  `db:"block_id"`
+	TokenAddress       string `db:"token_address"`
+	TokenHolderAddress string `db:"token_holder_address"`
+}
+
+type TokenAllowanceDBRow struct {
+	ID                  int64  `db:"id"`
+	Allowance           int64  `db:"allowance"`
+	BlockID             int64  `db:"block_id"`
+	TokenAddress        string `db:"token_address"`
+	TokenHolderAddress  string `db:"token_holder_address"`
+	TokenSpenderAddress string `db:"token_spender_address"`
+}
+
 type TransferDBRow struct {
 	ID             int64 `db:"id"`
 	VulcanizeLogID int64 `db:"vulcanize_log_id"`
@@ -40,7 +57,7 @@ func CreateNewDatabase() *postgres.DB {
 	node = core.Node{
 		GenesisBlock: "GENESIS",
 		NetworkID:    1,
-		ID:           "b6f90c0fdd8ec9607aed8ee45c69322e47b7063f0bfb7a29c8ecafab24d0a22d24dd2329b5ee6ed4125a03cb14e57fd584e67f9e53e6c631055cbbd82f080845",
+		ID:           "2ea672a45c4c7b96e3c4b130b21a22af390a552fd0b3cff96420b4bda26568d470dc56e05e453823f64f2556a6e4460ad1d4d00eb2d8b8fc16fcb1be73e86522",
 		ClientName:   "Geth/v1.7.2-stable-1db4ecdc/darwin-amd64/go1.9",
 	}
 	db := test_config.NewTestDB(node)
