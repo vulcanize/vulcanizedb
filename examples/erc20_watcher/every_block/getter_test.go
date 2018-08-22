1d50a0ace0c95cd28f6228ca2a346030ec2c598b
Ian Norden <iannordenn@gmail.com>
2018-08-22 13:10:23 -0500

    fixed tests for getting balanceOf and allowance, added balanceOf and allowance to the repo and repo tests and updated sql schema with new token_balance and token_allowance tables to accept this data

--- examples/erc20_watcher/every_block/getter_test.go
+++ examples/erc20_watcher/every_block/getter_test.go
@@ -22,6 +22,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/vulcanize/vulcanizedb/examples/constants"
 	"github.com/vulcanize/vulcanizedb/examples/erc20_watcher/every_block"
 	"github.com/vulcanize/vulcanizedb/pkg/fakes"
@@ -32,7 +33,7 @@ import (
 )
 
 var _ = Describe("ERC20 Getter", func() {
-	blockNumber := int64(5502914)
+	blockNumber := int64(6194634)
 
 	Describe("totalSupply", func() {
 		It("gets total supply data from the blockchain with the correct arguments", func() {
@@ -64,8 +65,8 @@ var _ = Describe("ERC20 Getter", func() {
 
 			Expect(err).NotTo(HaveOccurred())
 			expectedResult := big.Int{}
-			expectedResult.SetString("27647235749155415536952630", 10)
-			Expect(result).To(Equal(expectedResult))
+			expectedResult.SetString("47327413946297204537985606", 10)
+			Expect(result.String()).To(Equal(expectedResult.String()))
 		})
 
 		It("returns an error if the call to the blockchain fails", func() {
@@ -77,6 +78,134 @@ var _ = Describe("ERC20 Getter", func() {
 			Expect(result.String()).To(Equal("0"))
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("totalSupply"))
+			Expect(err.Error()).To(ContainSubstring(fakes.FakeError.Error()))
+		})
+	})
+
+	Describe("balanceOf", func() {
+		It("gets balance of a token holder address at a token contract address from the blockchain with the correct arguments", func() {
+			fakeBlockChain := fakes.NewMockBlockChain()
+			testGetter := every_block.NewGetter(fakeBlockChain)
+			testAbi := "testAbi"
+			testContractAddress := "testContractAddress"
+
+			testTokenHolderAddress := common.HexToAddress("0x2cccc4b4708b318a6290511aac75d6c3dbe0cf9f")
+			hashArgs := []common.Address{testTokenHolderAddress}
+			balanceOfArgs := make([]interface{}, len(hashArgs))
+			for i, s := range hashArgs {
+				balanceOfArgs[i] = s
+			}
+
+			_, err := testGetter.GetBalance(testAbi, testContractAddress, blockNumber, balanceOfArgs)
+
+			Expect(err).NotTo(HaveOccurred())
+			expectedResult := big.Int{}
+			expected := &expectedResult
+			fakeBlockChain.AssertFetchContractDataCalledWith(testAbi, testContractAddress, "balanceOf", balanceOfArgs, &expected, blockNumber)
+		})
+
+		It("gets a token holder address's balance on the dai contract at the given block height", func() {
+			infuraIPC := "https://mainnet.infura.io/J5Vd2fRtGsw0zZ0Ov3BL"
+			rawRpcClient, err := rpc.Dial(infuraIPC)
+			Expect(err).NotTo(HaveOccurred())
+			rpcClient := client.NewRpcClient(rawRpcClient, infuraIPC)
+			ethClient := ethclient.NewClient(rawRpcClient)
+			blockChainClient := client.NewEthClient(ethClient)
+			node := node.MakeNode(rpcClient)
+			transactionConverter := rpc2.NewRpcTransactionConverter(ethClient)
+			blockChain := geth.NewBlockChain(blockChainClient, node, transactionConverter)
+			realGetter := every_block.NewGetter(blockChain)
+
+			testTokenHolderAddress := common.HexToAddress("0x2cccc4b4708b318a6290511aac75d6c3dbe0cf9f")
+			hashArgs := []common.Address{testTokenHolderAddress}
+			balanceOfArgs := make([]interface{}, len(hashArgs))
+			for i, s := range hashArgs {
+				balanceOfArgs[i] = s
+			}
+
+			result, err := realGetter.GetBalance(constants.DaiAbiString, constants.DaiContractAddress, blockNumber, balanceOfArgs)
+
+			Expect(err).NotTo(HaveOccurred())
+			expectedResult := big.Int{}
+			expectedResult.SetString("1000000000000000000000000", 10)
+			Expect(result.String()).To(Equal(expectedResult.String()))
+		})
+
+		It("returns an error if the call to the blockchain fails", func() {
+			blockChain := fakes.NewMockBlockChain()
+			blockChain.SetFetchContractDataErr(fakes.FakeError)
+			errorGetter := every_block.NewGetter(blockChain)
+			result, err := errorGetter.GetBalance("", "", 0, nil)
+
+			Expect(result.String()).To(Equal("0"))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("balanceOf"))
+			Expect(err.Error()).To(ContainSubstring(fakes.FakeError.Error()))
+		})
+	})
+
+	Describe("allowance", func() {
+		It("gets allowance data from the blockchain with the correct arguments", func() {
+			fakeBlockChain := fakes.NewMockBlockChain()
+			testGetter := every_block.NewGetter(fakeBlockChain)
+			testAbi := "testAbi"
+			testContractAddress := "testContractAddress"
+
+			testTokenHolderAddress := common.HexToAddress("0x2cccc4b4708b318a6290511aac75d6c3dbe0cf9f")
+			testTokenSpenderAddress := common.HexToAddress("0x2cccc4b4708b318a6290511aac75d6c3dbe0cf9f")
+
+			hashArgs := []common.Address{testTokenHolderAddress, testTokenSpenderAddress}
+			allowanceArgs := make([]interface{}, len(hashArgs))
+			for i, s := range hashArgs {
+				allowanceArgs[i] = s
+			}
+
+			_, err := testGetter.GetAllowance(testAbi, testContractAddress, blockNumber, allowanceArgs)
+
+			Expect(err).NotTo(HaveOccurred())
+			expectedResult := big.Int{}
+			expected := &expectedResult
+			fakeBlockChain.AssertFetchContractDataCalledWith(testAbi, testContractAddress, "allowance", allowanceArgs, &expected, blockNumber)
+		})
+
+		It("gets the allowance for a spending address and holder address on the dai contract at the given block height", func() {
+			infuraIPC := "https://mainnet.infura.io/J5Vd2fRtGsw0zZ0Ov3BL"
+			rawRpcClient, err := rpc.Dial(infuraIPC)
+			Expect(err).NotTo(HaveOccurred())
+			rpcClient := client.NewRpcClient(rawRpcClient, infuraIPC)
+			ethClient := ethclient.NewClient(rawRpcClient)
+			blockChainClient := client.NewEthClient(ethClient)
+			node := node.MakeNode(rpcClient)
+			transactionConverter := rpc2.NewRpcTransactionConverter(ethClient)
+			blockChain := geth.NewBlockChain(blockChainClient, node, transactionConverter)
+			realGetter := every_block.NewGetter(blockChain)
+
+			testTokenHolderAddress := common.HexToAddress("0x2cccc4b4708b318a6290511aac75d6c3dbe0cf9f")
+			testTokenSpenderAddress := common.HexToAddress("0x2cccc4b4708b318a6290511aac75d6c3dbe0cf9f")
+
+			hashArgs := []common.Address{testTokenHolderAddress, testTokenSpenderAddress}
+			allowanceArgs := make([]interface{}, len(hashArgs))
+			for i, s := range hashArgs {
+				allowanceArgs[i] = s
+			}
+
+			result, err := realGetter.GetAllowance(constants.DaiAbiString, constants.DaiContractAddress, blockNumber, allowanceArgs)
+
+			Expect(err).NotTo(HaveOccurred())
+			expectedResult := big.Int{}
+			expectedResult.SetString("0", 10)
+			Expect(result.String()).To(Equal(expectedResult.String()))
+		})
+
+		It("returns an error if the call to the blockchain fails", func() {
+			blockChain := fakes.NewMockBlockChain()
+			blockChain.SetFetchContractDataErr(fakes.FakeError)
+			errorGetter := every_block.NewGetter(blockChain)
+			result, err := errorGetter.GetAllowance("", "", 0, nil)
+
+			Expect(result.String()).To(Equal("0"))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("allowance"))
 			Expect(err.Error()).To(ContainSubstring(fakes.FakeError.Error()))
 		})
 	})
