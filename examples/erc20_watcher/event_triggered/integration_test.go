496d20a9077410ad5df6408478ce44de72a6eb18
Ian Norden <iannordenn@gmail.com>
2018-08-28 16:03:55 -0500

    event_triggered transformer, transformer tests, and integrations tests for Transfer and Approval events

--- examples/erc20_watcher/event_triggered/integration_test.go
+++ examples/erc20_watcher/event_triggered/integration_test.go
@@ -14,44 +14,54 @@
 
 package event_triggered_test
 
-/*
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/vulcanize/vulcanizedb/pkg/core"
-	"github.com/vulcanize/vulcanizedb/pkg/config"
 	"github.com/vulcanize/vulcanizedb/examples/constants"
 	"github.com/vulcanize/vulcanizedb/examples/erc20_watcher/event_triggered"
-	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres/repositories"
+	"github.com/vulcanize/vulcanizedb/examples/generic"
+	"github.com/vulcanize/vulcanizedb/examples/test_helpers"
+	"github.com/vulcanize/vulcanizedb/pkg/config"
+	"github.com/vulcanize/vulcanizedb/pkg/core"
 	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres"
-	"github.com/vulcanize/vulcanizedb/pkg/fakes"
+	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres/repositories"
+	"math/rand"
 	"time"
 )
 
-var idOne = "0x000000000000000000000000000000000000000000000000000000000000af21"
-var logKill = core.Log{
+var transferLog = core.Log{
 	BlockNumber: 5488076,
 	Address:     constants.DaiContractAddress,
 	TxHash:      "0x135391a0962a63944e5908e6fedfff90fb4be3e3290a21017861099bad6546ae",
-	Index:       0,
+	Index:       110,
 	Topics: [4]string{
 		constants.TransferEventSignature,
-		idOne,
+		"0x000000000000000000000000000000000000000000000000000000000000af21",
 		"0x9dd48110dcc444fdc242510c09bbbbe21a5975cac061d82f7b843bce061ba391",
-		"0x0000000000000000000000003dc389e0a69d6364a66ab64ebd51234da9569284",
+		"",
 	},
 	Data: "0x000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc200000000000000000000000089d24a6b4ccb1b6faa2625fe562bdd9a23260359000000000000000000000000000000000000000000000000392d2e2bda9c00000000000000000000000000000000000000000000000000927f41fa0a4a418000000000000000000000000000000000000000000000000000000000005adcfebe",
 }
 
-var expectedLogKill = event_triggered.TransferModel{
-	Block:     5488076,
-	TxHash:    "0x135391a0962a63944e5908e6fedfff90fb4be3e3290a21017861099bad6546ae",
+var approvalLog = core.Log{
+	BlockNumber: 5488076,
+	Address:     constants.DaiContractAddress,
+	TxHash:      "0x135391a0962a63944e5908e6fedfff90fb4be3e3290a21017861099bad6546ae",
+	Index:       110,
+	Topics: [4]string{
+		constants.ApprovalEventSignature,
+		"0x000000000000000000000000000000000000000000000000000000000000af21",
+		"0x9dd48110dcc444fdc242510c09bbbbe21a5975cac061d82f7b843bce061ba391",
+		"",
+	},
+	Data: "0x000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc200000000000000000000000089d24a6b4ccb1b6faa2625fe562bdd9a23260359000000000000000000000000000000000000000000000000392d2e2bda9c00000000000000000000000000000000000000000000000000927f41fa0a4a418000000000000000000000000000000000000000000000000000000000005adcfebe",
 }
 
-//converted logID to assert against
+//converted transfer to assert against
 var logs = []core.Log{
-	logKill,
+	transferLog,
+	approvalLog,
 	{
 		BlockNumber: 0,
 		TxHash:      "",
@@ -64,6 +74,7 @@ var logs = []core.Log{
 
 var _ = Describe("Integration test with vulcanizedb", func() {
 	var db *postgres.DB
+	rand.Seed(time.Now().UnixNano())
 
 	BeforeEach(func() {
 		var err error
@@ -73,18 +84,31 @@ var _ = Describe("Integration test with vulcanizedb", func() {
 			Port:     5432,
 		}, core.Node{})
 		Expect(err).NotTo(HaveOccurred())
-		lr := repositories.LogRepository{DB: db}
-		err = lr.CreateLogs(logs)
+
+		receiptRepository := repositories.ReceiptRepository{DB: db}
+		blockRepository := *repositories.NewBlockRepository(db)
+
+		blockNumber := rand.Int63()
+		blockId := test_helpers.CreateBlock(blockNumber, blockRepository)
+
+		receipt := core.Receipt{
+			Logs: logs,
+		}
+		receipts := []core.Receipt{receipt}
+
+		err = receiptRepository.CreateReceiptsAndLogs(blockId, receipts)
 		Expect(err).ToNot(HaveOccurred())
 
 		var vulcanizeLogIds []int64
-		err = db.Select(&vulcanizeLogIds, `SELECT id FROM public.logs`)
+		err = db.Select(&vulcanizeLogIds, `SELECT id FROM logs`)
 		Expect(err).ToNot(HaveOccurred())
 
 	})
 
 	AfterEach(func() {
-		_, err := db.Exec(`DELETE FROM oasis.kill`)
+		_, err := db.Exec(`DELETE FROM token_transfers`)
+		Expect(err).ToNot(HaveOccurred())
+		_, err = db.Exec(`DELETE FROM token_approvals`)
 		Expect(err).ToNot(HaveOccurred())
 		_, err = db.Exec(`DELETE FROM log_filters`)
 		Expect(err).ToNot(HaveOccurred())
@@ -92,36 +116,58 @@ var _ = Describe("Integration test with vulcanizedb", func() {
 		Expect(err).ToNot(HaveOccurred())
 	})
 
-	It("creates oasis.kill for each LogKill event received", func() {
-		blockchain := &fakes.MockBlockChain{}
-		transformer := event_triggered.NewTransformer(db, blockchain)
+	It("creates transfer entry for each Transfer event received", func() {
+		transformer := event_triggered.NewTransformer(db, generic.DaiConfig)
 
 		transformer.Execute()
 
 		var count int
-		err := db.QueryRow(`SELECT COUNT(*) FROM oasis.kill`).Scan(&count)
+		err := db.QueryRow(`SELECT COUNT(*) FROM token_transfers`).Scan(&count)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(count).To(Equal(1))
 
 		type dbRow struct {
-			DBID           uint64 `db:"db_id"`
+			DBID           uint64 `db:"id"`
 			VulcanizeLogID int64  `db:"vulcanize_log_id"`
 			event_triggered.TransferModel
 		}
-		var logKill dbRow
-		err = db.Get(&logKill, `SELECT * FROM oasis.kill WHERE block=$1`, logs[0].BlockNumber)
+		var transfer dbRow
+		err = db.Get(&transfer, `SELECT * FROM token_transfers WHERE block=$1`, logs[0].BlockNumber)
 		Expect(err).ToNot(HaveOccurred())
-		Expect(logKill.ID).To(Equal(expectedLogKill.ID))
-		Expect(logKill.Pair).To(Equal(expectedLogKill.Pair))
-		Expect(logKill.Guy).To(Equal(expectedLogKill.Guy))
-		Expect(logKill.Gem).To(Equal(expectedLogKill.Gem))
-		Expect(logKill.Lot).To(Equal(expectedLogKill.Lot))
-		Expect(logKill.Pie).To(Equal(expectedLogKill.Pie))
-		Expect(logKill.Bid).To(Equal(expectedLogKill.Bid))
-		Expect(logKill.Block).To(Equal(expectedLogKill.Block))
-		Expect(logKill.Tx).To(Equal(expectedLogKill.Tx))
-		Expect(logKill.Timestamp.Equal(expectedLogKill.Timestamp)).To(BeTrue())
+		Expect(transfer.TokenName).To(Equal(expectedTransferModel.TokenName))
+		Expect(transfer.TokenAddress).To(Equal(expectedTransferModel.TokenAddress))
+		Expect(transfer.To).To(Equal(expectedTransferModel.To))
+		Expect(transfer.From).To(Equal(expectedTransferModel.From))
+		Expect(transfer.Tokens).To(Equal(expectedTransferModel.Tokens))
+		Expect(transfer.Block).To(Equal(expectedTransferModel.Block))
+		Expect(transfer.TxHash).To(Equal(expectedTransferModel.TxHash))
+	})
+
+	It("creates approval entry for each Approval event received", func() {
+		transformer := event_triggered.NewTransformer(db, generic.DaiConfig)
+
+		transformer.Execute()
+
+		var count int
+		err := db.QueryRow(`SELECT COUNT(*) FROM token_approvals`).Scan(&count)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(count).To(Equal(1))
+
+		type dbRow struct {
+			DBID           uint64 `db:"id"`
+			VulcanizeLogID int64  `db:"vulcanize_log_id"`
+			event_triggered.ApprovalModel
+		}
+		var transfer dbRow
+		err = db.Get(&transfer, `SELECT * FROM token_approvals WHERE block=$1`, logs[0].BlockNumber)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(transfer.TokenName).To(Equal(expectedApprovalModel.TokenName))
+		Expect(transfer.TokenAddress).To(Equal(expectedApprovalModel.TokenAddress))
+		Expect(transfer.Owner).To(Equal(expectedApprovalModel.Owner))
+		Expect(transfer.Spender).To(Equal(expectedApprovalModel.Spender))
+		Expect(transfer.Tokens).To(Equal(expectedApprovalModel.Tokens))
+		Expect(transfer.Block).To(Equal(expectedApprovalModel.Block))
+		Expect(transfer.TxHash).To(Equal(expectedApprovalModel.TxHash))
 	})
 
 })
-*/
--- examples/erc20_watcher/event_triggered/transformer.go
+++ examples/erc20_watcher/event_triggered/transformer.go
@@ -13,3 +13,69 @@
 // limitations under the License.
 
 package event_triggered
+
+import (
+	"log"
+
+	"fmt"
+	"github.com/vulcanize/vulcanizedb/examples/constants"
+	"github.com/vulcanize/vulcanizedb/examples/generic"
+	"github.com/vulcanize/vulcanizedb/libraries/shared"
+	"github.com/vulcanize/vulcanizedb/pkg/datastore"
+	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres"
+	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres/repositories"
+)
+
+type DaiTransformer struct {
+	Converter              ERC20ConverterInterface
+	WatchedEventRepository datastore.WatchedEventRepository
+	FilterRepository       datastore.FilterRepository
+	Repository             Datastore
+}
+
+func NewTransformer(db *postgres.DB, config generic.ContractConfig) shared.Transformer {
+	var transformer shared.Transformer
+	cnvtr := NewERC20Converter(config)
+	wer := repositories.WatchedEventRepository{DB: db}
+	fr := repositories.FilterRepository{DB: db}
+	lkr := Repository{DB: db}
+	transformer = &DaiTransformer{
+		Converter:              cnvtr,
+		WatchedEventRepository: wer,
+		FilterRepository:       fr,
+		Repository:             lkr,
+	}
+	for _, filter := range constants.DaiFilters {
+		fr.CreateFilter(filter)
+	}
+	return transformer
+}
+
+func (tr DaiTransformer) Execute() error {
+	for _, filter := range constants.DaiFilters {
+		watchedEvents, err := tr.WatchedEventRepository.GetWatchedEvents(filter.Name)
+		if err != nil {
+			log.Println(fmt.Sprintf("Error fetching events for %s:", filter.Name), err)
+			return err
+		}
+		for _, we := range watchedEvents {
+			if filter.Name == "Transfer" {
+				entity, err := tr.Converter.ToTransferEntity(*we)
+				model := tr.Converter.ToTransferModel(*entity)
+				if err != nil {
+					log.Printf("Error persisting data for Dai Transfers (watchedEvent.LogID %d):\n %s", we.LogID, err)
+				}
+				tr.Repository.CreateTransfer(model, we.LogID)
+			}
+			if filter.Name == "Approval" {
+				entity, err := tr.Converter.ToApprovalEntity(*we)
+				model := tr.Converter.ToApprovalModel(*entity)
+				if err != nil {
+					log.Printf("Error persisting data for Dai Approvals (watchedEvent.LogID %d):\n %s", we.LogID, err)
+				}
+				tr.Repository.CreateApproval(model, we.LogID)
+			}
+		}
+	}
+	return nil
+}
