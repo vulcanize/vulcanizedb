552fedc76726312b152707088c25338ada32ee7e
Taka Goto <tgoto@8thlight.com>
2018-09-04 15:10:15 -0500

    add datadog monitoring for vdb

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"github.com/vulcanize/vulcanizedb/cmd"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	tracer.Start(tracer.WithServiceName(viper.GetString("datadog.name")))
+
 	cmd.Execute()
+
+	defer tracer.Stop()
 }
