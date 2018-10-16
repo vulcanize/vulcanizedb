2cc303a2f5679f8b377ba9688736c8e1a68512a8
Edvard <edvard@hubinette.me>
2018-10-16 11:35:05 +0200

    Refactor stability_fee transfomer

--- pkg/transformers/pit_file/stability_fee/repository.go
+++ pkg/transformers/pit_file/stability_fee/repository.go
@@ -19,51 +19,46 @@ import (
 	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres"
 )
 
-type Repository interface {
-	Create(headerID int64, models []PitFileStabilityFeeModel) error
-	MarkHeaderChecked(headerID int64) error
-	MissingHeaders(startingBlockNumber, endingBlockNumber int64) ([]core.Header, error)
-}
-
 type PitFileStabilityFeeRepository struct {
-	db *postgres.DB
+	DB *postgres.DB
 }
 
-func NewPitFileStabilityFeeRepository(db *postgres.DB) PitFileStabilityFeeRepository {
-	return PitFileStabilityFeeRepository{
-		db: db,
-	}
-}
-
-func (repository PitFileStabilityFeeRepository) Create(headerID int64, models []PitFileStabilityFeeModel) error {
-	tx, err := repository.db.Begin()
+func (repository PitFileStabilityFeeRepository) Create(headerID int64, models []interface{}) error {
+	tx, err := repository.DB.Begin()
 	if err != nil {
 		return err
 	}
+
+	var pitFileSF PitFileStabilityFeeModel
 	for _, model := range models {
+		pitFileSF = model.(PitFileStabilityFeeModel)
 		_, err = tx.Exec(
 			`INSERT into maker.pit_file_stability_fee (header_id, what, data, tx_idx, raw_log)
         VALUES($1, $2, $3, $4, $5)`,
-			headerID, model.What, model.Data, model.TransactionIndex, model.Raw,
+			headerID, pitFileSF.What, pitFileSF.Data, pitFileSF.TransactionIndex, pitFileSF.Raw,
 		)
+
 		if err != nil {
 			tx.Rollback()
 			return err
 		}
 	}
+
 	_, err = tx.Exec(`INSERT INTO public.checked_headers (header_id, pit_file_stability_fee_checked)
 		VALUES ($1, $2) 
 	ON CONFLICT (header_id) DO
 		UPDATE SET pit_file_stability_fee_checked = $2`, headerID, true)
+
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
+
 	return tx.Commit()
 }
 
 func (repository PitFileStabilityFeeRepository) MarkHeaderChecked(headerID int64) error {
-	_, err := repository.db.Exec(`INSERT INTO public.checked_headers (header_id, pit_file_stability_fee_checked)
+	_, err := repository.DB.Exec(`INSERT INTO public.checked_headers (header_id, pit_file_stability_fee_checked)
 		VALUES ($1, $2) 
 	ON CONFLICT (header_id) DO
 		UPDATE SET pit_file_stability_fee_checked = $2`, headerID, true)
@@ -72,7 +67,7 @@ func (repository PitFileStabilityFeeRepository) MarkHeaderChecked(headerID int64
 
 func (repository PitFileStabilityFeeRepository) MissingHeaders(startingBlockNumber, endingBlockNumber int64) ([]core.Header, error) {
 	var result []core.Header
-	err := repository.db.Select(
+	err := repository.DB.Select(
 		&result,
 		`SELECT headers.id, headers.block_number FROM headers
                LEFT JOIN checked_headers on headers.id = header_id
@@ -82,7 +77,11 @@ func (repository PitFileStabilityFeeRepository) MissingHeaders(startingBlockNumb
                AND headers.eth_node_fingerprint = $3`,
 		startingBlockNumber,
 		endingBlockNumber,
-		repository.db.Node.ID,
+		repository.DB.Node.ID,
 	)
 	return result, err
+}
+
+func (repository *PitFileStabilityFeeRepository) SetDB(db *postgres.DB) {
+	repository.DB = db
 }
