14a453e4ddec4a5a6eb414f3279bf30b95e01670
Rob Mulholand <rmulholand@8thlight.com>
2018-10-17 15:31:20 -0500

    Add Vat init integration test

---
+++ pkg/transformers/integration_tests/vat_init.go
@@ -0,0 +1,56 @@
+// Copyright 2018 Vulcanize
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration_tests
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/vulcanize/vulcanizedb/pkg/transformers/vat_init"
+	"github.com/vulcanize/vulcanizedb/test_config"
+)
+
+var _ = Describe("VatInit Transformer", func() {
+	It("transforms VatInit log events", func() {
+		blockNumber := int64(8535561)
+		config := vat_init.VatInitConfig
+		config.StartingBlockNumber = blockNumber
+		config.EndingBlockNumber = blockNumber
+
+		rpcClient, ethClient, err := getClients(ipc)
+		Expect(err).NotTo(HaveOccurred())
+		blockchain, err := getBlockChain(rpcClient, ethClient)
+		Expect(err).NotTo(HaveOccurred())
+
+		db := test_config.NewTestDB(blockchain.Node())
+		test_config.CleanTestDB(db)
+
+		err = persistHeader(rpcClient, db, blockNumber)
+		Expect(err).NotTo(HaveOccurred())
+
+		initializer := vat_init.VatInitTransformerInitializer{Config: config}
+		transformer := initializer.NewVatInitTransformer(db, blockchain)
+		err = transformer.Execute()
+		Expect(err).NotTo(HaveOccurred())
+
+		var dbResults []vat_init.VatInitModel
+		err = db.Select(&dbResults, `SELECT ilk from maker.vat_init`)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(len(dbResults)).To(Equal(1))
+		dbResult := dbResults[0]
+		Expect(dbResult.Ilk).To(Equal("ETH"))
+	})
+})
