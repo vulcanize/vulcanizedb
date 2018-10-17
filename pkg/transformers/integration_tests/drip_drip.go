c6988d4cb4e6fd5515803917641abbc6793dc806
Rob Mulholand <rmulholand@8thlight.com>
2018-10-17 16:33:23 -0500

    Add Drip drip integration test

---
+++ pkg/transformers/integration_tests/drip_drip.go
@@ -0,0 +1,56 @@
+// Copyright 2018 Vulcanize
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration_tests
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/vulcanize/vulcanizedb/pkg/transformers/drip_drip"
+	"github.com/vulcanize/vulcanizedb/test_config"
+)
+
+var _ = Describe("DripDrip Transformer", func() {
+	It("transforms DripDrip log events", func() {
+		blockNumber := int64(8934775)
+		config := drip_drip.DripDripConfig
+		config.StartingBlockNumber = blockNumber
+		config.EndingBlockNumber = blockNumber
+
+		rpcClient, ethClient, err := getClients(ipc)
+		Expect(err).NotTo(HaveOccurred())
+		blockchain, err := getBlockChain(rpcClient, ethClient)
+		Expect(err).NotTo(HaveOccurred())
+
+		db := test_config.NewTestDB(blockchain.Node())
+		test_config.CleanTestDB(db)
+
+		err = persistHeader(rpcClient, db, blockNumber)
+		Expect(err).NotTo(HaveOccurred())
+
+		initializer := drip_drip.DripDripTransformerInitializer{Config: config}
+		transformer := initializer.NewDripDripTransformer(db, blockchain)
+		err = transformer.Execute()
+		Expect(err).NotTo(HaveOccurred())
+
+		var dbResults []drip_drip.DripDripModel
+		err = db.Select(&dbResults, `SELECT ilk from maker.drip_drip`)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(len(dbResults)).To(Equal(1))
+		dbResult := dbResults[0]
+		Expect(dbResult.Ilk).To(Equal("ETH"))
+	})
+})
