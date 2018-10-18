52b9dc4c41e60331ea33aedd6f5fddc93ceabf3b
Rob Mulholand <rmulholand@8thlight.com>
2018-10-18 13:34:53 -0500

    Add Pit file ilk integration test

---
+++ pkg/transformers/integration_tests/pit_file_ilk.go
@@ -0,0 +1,58 @@
+// Copyright 2018 Vulcanize
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration_tests
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/vulcanize/vulcanizedb/pkg/transformers/pit_file"
+	"github.com/vulcanize/vulcanizedb/pkg/transformers/pit_file/ilk"
+	"github.com/vulcanize/vulcanizedb/test_config"
+)
+
+var _ = Describe("PitFileIlk Transformer", func() {
+	It("fetches and transforms a PitFileIlk event from Kovan chain", func() {
+		blockNumber := int64(9103223)
+		config := pit_file.PitFileConfig
+		config.StartingBlockNumber = blockNumber
+		config.EndingBlockNumber = blockNumber
+
+		rpcClient, ethClient, err := getClients(ipc)
+		Expect(err).NotTo(HaveOccurred())
+		blockchain, err := getBlockChain(rpcClient, ethClient)
+		Expect(err).NotTo(HaveOccurred())
+
+		db := test_config.NewTestDB(blockchain.Node())
+		test_config.CleanTestDB(db)
+
+		err = persistHeader(rpcClient, db, blockNumber)
+		Expect(err).NotTo(HaveOccurred())
+
+		initializer := ilk.PitFileIlkTransformerInitializer{Config: config}
+		transformer := initializer.NewPitFileIlkTransformer(db, blockchain)
+		err = transformer.Execute()
+		Expect(err).NotTo(HaveOccurred())
+
+		var dbResult []ilk.PitFileIlkModel
+		err = db.Select(&dbResult, `SELECT ilk, what, data from maker.pit_file_ilk`)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(len(dbResult)).To(Equal(1))
+		Expect(dbResult[0].Ilk).To(Equal("ETH"))
+		Expect(dbResult[0].What).To(Equal("spot"))
+		Expect(dbResult[0].Data).To(Equal("139840000000000000000000000000"))
+	})
+})
