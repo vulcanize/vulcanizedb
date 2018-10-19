d30089eeaaf3899e8f72aa02efcd20b73cef2480
Rob Mulholand <rmulholand@8thlight.com>
2018-10-19 11:24:46 -0500

    Add log index to Deal

--- db/migrations/1536615440_create_deal_table.up.sql
+++ db/migrations/1536615440_create_deal_table.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE maker.deal (
-  id            SERIAL PRIMARY KEY,
-  header_id     INTEGER NOT NULL REFERENCES headers (id) ON DELETE CASCADE,
-  bid_id        NUMERIC NOT NULL,
-	tx_idx        INTEGER NOT NUll,
-  raw_log       JSONB,
-  UNIQUE (header_id, tx_idx)
+  id                SERIAL PRIMARY KEY,
+  header_id         INTEGER NOT NULL REFERENCES headers (id) ON DELETE CASCADE,
+  bid_id            NUMERIC NOT NULL,
+  contract_address  VARCHAR,
+  log_idx           INTEGER NOT NUll,
+  tx_idx            INTEGER NOT NUll,
+  raw_log           JSONB,
+  UNIQUE (header_id, tx_idx, log_idx)
 );
