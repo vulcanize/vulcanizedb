77c7c9b453f7723aa356bddc90ddb0e28ce35b80
Edvard <edvard@hubinette.me>
2018-10-26 15:26:36 +0200

    Squash frob checked_headers

--- db/migrations/1533844125_create_frob.up.sql
+++ db/migrations/1533844125_create_frob.up.sql
@@ -13,3 +13,6 @@ CREATE TABLE maker.frob (
   raw_log   JSONB,
   UNIQUE (header_id, tx_idx, log_idx)
 );
+
+ALTER TABLE public.checked_headers
+  ADD COLUMN frob_checked BOOLEAN NOT NULL DEFAULT FALSE;
