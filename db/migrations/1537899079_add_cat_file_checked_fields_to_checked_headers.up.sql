26888a72522cb7e538bba4e53821cbec539ebd9a
Edvard <edvard@hubinette.me>
2018-10-26 15:25:27 +0200

    Squash cat_file checked_headers

--- db/migrations/1537899079_add_cat_file_checked_fields_to_checked_headers.up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE public.checked_headers
-    ADD COLUMN cat_file_chop_lump_checked BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE public.checked_headers
-    ADD COLUMN cat_file_flip_checked BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE public.checked_headers
-    ADD COLUMN cat_file_pit_vow_checked BOOLEAN NOT NULL DEFAULT FALSE;
