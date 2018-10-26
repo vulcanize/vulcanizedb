48525203f30a831ee8b0ba5a47deb85bcb966da1
Edvard <edvard@hubinette.me>
2018-10-26 15:17:30 +0200

    Squash dent checked_headers

--- db/migrations/1534799167_create_dent.up.sql
+++ db/migrations/1534799167_create_dent.up.sql
@@ -11,3 +11,6 @@ CREATE TABLE maker.dent (
   raw_log          JSONB,
   UNIQUE (header_id, tx_idx, log_idx)
 );
+
+ALTER TABLE public.checked_headers
+  ADD COLUMN dent_checked BOOLEAN NOT NULL DEFAULT FALSE;
