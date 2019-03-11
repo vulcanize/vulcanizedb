efeee7bb809649b4b12a5ec1a72ef0cc8867b712
Ian Norden <iannordenn@gmail.com>
2019-03-11 16:18:13 -0500

    update © to 2019

--- libraries/shared/transformer/generic_transformer.go
+++ libraries/shared/transformer/generic_transformer.go
@@ -1,5 +1,5 @@
 // VulcanizeDB
-// Copyright © 2018 Vulcanize
+// Copyright © 2019 Vulcanize
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
