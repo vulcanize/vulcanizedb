ca354de229510537ff4115e9032c2860b71c9157
Rob Mulholand <rmulholand@8thlight.com>
2019-04-30 11:58:27 -0500

    (VDB-563) Remove Postgraphile bespoke code
    - Add documentation for running Postgraphile from their binary

--- postgraphile/src/adapters/http.ts
+++
@@ -1,7 +0,0 @@
-import { IncomingMessage, ServerResponse, Server } from 'http';
-
-export type RequestListenerCallback
-  = (request: IncomingMessage, response: ServerResponse) => void;
-
-export type CreateHttpServerCallback
-  = (requestListener?: RequestListenerCallback) => Server;
