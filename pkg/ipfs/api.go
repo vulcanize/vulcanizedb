6868ffbd9b90e9b0915160f7a34fadd87e5c2005
Ian Norden <iannordenn@gmail.com>
2019-05-17 01:27:02 -0500

    pub sub service and api

---
+++ pkg/ipfs/api.go
@@ -0,0 +1,82 @@
+// VulcanizeDB
+// Copyright © 2019 Vulcanize
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package ipfs
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+// APIName is the namespace used for the state diffing service API
+const APIName = "vulcanizedb"
+
+// APIVersion is the version of the state diffing service API
+const APIVersion = "0.0.1"
+
+// PublicSeedNodeAPI
+type PublicSeedNodeAPI struct {
+	snp SyncPublishAndServe
+}
+
+// NewPublicSeedNodeAPI
+func NewPublicSeedNodeAPI(snp SyncPublishAndServe) *PublicSeedNodeAPI {
+	return &PublicSeedNodeAPI{
+		snp: snp,
+	}
+}
+
+// Subscribe is the public method to setup a subscription that fires off state-diff payloads as they are created
+func (api *PublicSeedNodeAPI) Subscribe(ctx context.Context, params *Params) (*rpc.Subscription, error) {
+	// ensure that the RPC connection supports subscriptions
+	notifier, supported := rpc.NotifierFromContext(ctx)
+	if !supported {
+		return nil, rpc.ErrNotificationsUnsupported
+	}
+
+	// create subscription and start waiting for statediff events
+	rpcSub := notifier.CreateSubscription()
+
+	go func() {
+		// subscribe to events from the state diff service
+		payloadChannel := make(chan ResponsePayload)
+		quitChan := make(chan bool)
+		api.snp.Subscribe(rpcSub.ID, payloadChannel, quitChan, params)
+
+		// loop and await state diff payloads and relay them to the subscriber with then notifier
+		for {
+			select {
+			case packet := <-payloadChannel:
+				if err := notifier.Notify(rpcSub.ID, packet); err != nil {
+					log.Error("Failed to send state diff packet", "err", err)
+				}
+			case <-rpcSub.Err():
+				err := api.snp.Unsubscribe(rpcSub.ID)
+				if err != nil {
+					log.Error("Failed to unsubscribe from the state diff service", err)
+				}
+				return
+			case <-quitChan:
+				// don't need to unsubscribe, statediff service does so before sending the quit signal
+				return
+			}
+		}
+	}()
+
+	return rpcSub, nil
+}
