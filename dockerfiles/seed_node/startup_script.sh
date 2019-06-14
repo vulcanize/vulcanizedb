b76bdc51f22f121ced9c223e88457a35e923300b
Ian Norden <iannordenn@gmail.com>
2019-06-13 21:12:56 -0500

    fixing dockerfile and start up script

--- dockerfiles/seed_node/startup_script.sh
+++ dockerfiles/seed_node/startup_script.sh
@@ -1,37 +1,71 @@
-#!/usr/bin/env bash
-# Runs the migrations and starts the syncPublishScreenAndServe service
+#!/bin/sh
+# Runs the db migrations and starts the seed node services
 
 # Exit if the variable tests fail
 set -e
 
 # Check the database variables are set
-test $DATABASE_NAME
-test $DATABASE_HOSTNAME
-test $DATABASE_PORT
-test $DATABASE_USER
-test $DATABASE_PASSWORD
-
-# Export our database variables so that the IPFS Postgres plugin can use them
-export IPFS_PGHOST=$DATABASE_HOSTNAME
-export IPFS_PGUSER=$DATABASE_USER
-export IPFS_PGDATABASE=$DATABASE_NAME
-export IPFS_PGPORT=$DATABASE_PORT
-export IPFS_PGPASSWORD=$DATABASE_PASSWORD
-
-# Construct the connection string for postgres
-CONNECT_STRING=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable
-echo "Connecting with: $CONNECT_STRING"
-
+test $VDB_PG_NAME
+test $VDB_PG_HOSTNAME
+test $VDB_PG_PORT
+test $VDB_PG_USER
 set +e
 
+# Export our database variables so that the IPFS Postgres plugin can use them
+export IPFS_PGHOST=$VDB_PG_HOSTNAME
+export IPFS_PGUSER=$VDB_PG_USER
+export IPFS_PGDATABASE=$VDB_PG_NAME
+export IPFS_PGPORT=$VDB_PG_PORT
+export IPFS_PGPASSWORD=$VDB_PG_PASSWORD
+
+# Construct the connection string for postgres
+VDB_PG_CONNECT=postgresql://$VDB_PG_USER:$VDB_PG_PASSWORD@$VDB_PG_HOSTNAME:$VDB_PG_PORT/$VDB_PG_NAME?sslmode=disable
+
 # Run the DB migrations
-./goose postgres "$CONNECT_STRING" up
+echo "Connecting with: $VDB_PG_CONNECT"
+echo "Running database migrations"
+./goose -dir migrations/vulcanizedb postgres "$VDB_PG_CONNECT" up
+
+# If the db migrations ran without err
 if [ $? -eq 0 ]; then
-  # Fire up the services
-  ipfs ipfs init --profile=postgresds &
-  geth --statediff --statediff.streamblock --ws --syncmode=full &
-  ./vulcanizedb syncPublishScreenAndServe --config environments/seedNodeStaging.toml &
+    # Initialize PG-IPFS
+    echo "Initializing Postgres-IPFS profile"
+    ./ipfs init --profile=postgresds
 else
-  echo "Could not run migrations. Are the database details correct?"
+    echo "Could not run migrations. Are the database details correct?"
+    exit
 fi
+
+# If IPFS initialization was successful
+if [ $? -eq 0 ]; then
+    # Begin the state-diffing Geth process
+    echo "Beginning the state-diffing Geth process"
+    ./geth --statediff --statediff.streamblock --ws --syncmode=full 2>&1 | tee -a log.txt &
+    sleep 1
+else
+    echo "Could not initialize Postgres backed IPFS profile. Are the database details correct?"
+    exit
+fi
+
+# If Geth startup was successful
+if [ $? -eq 0 ]; then
+    # Wait until block synchronisation has begun
+    echo "Waiting for block synchronization to begin"
+    ( tail -f -n0 log.txt & ) | grep -q "Block synchronisation started" # this blocks til we see "Block synchronisation started"
+    # And then spin up the syncPublishScreenAndServe Vulcanizedb service
+    echo "Beginning the syncPublishScreenAndServe vulcanizedb process"
+    ./vulcanizedb syncPublishScreenAndServe --config=config.toml 2>&1 | tee -a log.txt &
+else
+    echo "Could not initialize Postgres backed IPFS profile. Are the database details correct?"
+    exit
+fi
+
+# If Vulcanizedb startup was successful
+if [ $? -eq 0 ]; then
+    echo "Seed node successfully booted"
+else
+    echo "Could not start vulcanizedb syncPublishScreenAndServe process. Is the config file correct?"
+    exit
+fi
+
 wait
