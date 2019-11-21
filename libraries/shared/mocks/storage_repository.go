5c9a054146951252d4781eaca361fad3b3cd450a
Rob Mulholand <rmulholand@8thlight.com>
2019-11-21 16:12:46 -0600

    Update storage repository to accept headerID
    - replaces block number and block hash
    - make corresponding update to storage transformer

--- libraries/shared/mocks/storage_repository.go
+++ libraries/shared/mocks/storage_repository.go
@@ -22,16 +22,14 @@ import (
 )
 
 type MockStorageRepository struct {
-	CreateErr         error
-	PassedBlockNumber int
-	PassedBlockHash   string
-	PassedMetadata    utils.StorageValueMetadata
-	PassedValue       interface{}
+	CreateErr      error
+	PassedHeaderID int64
+	PassedMetadata utils.StorageValueMetadata
+	PassedValue    interface{}
 }
 
-func (repository *MockStorageRepository) Create(blockNumber int, blockHash string, metadata utils.StorageValueMetadata, value interface{}) error {
-	repository.PassedBlockNumber = blockNumber
-	repository.PassedBlockHash = blockHash
+func (repository *MockStorageRepository) Create(headerID int64, metadata utils.StorageValueMetadata, value interface{}) error {
+	repository.PassedHeaderID = headerID
 	repository.PassedMetadata = metadata
 	repository.PassedValue = value
 	return repository.CreateErr
