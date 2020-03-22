1c208e5946bcd27e1f367dd6cedc2ba68bb0770c
Ian Norden <iannordenn@gmail.com>
2020-03-22 12:14:43 -0500

    docker-compose

--- dockerfiles/super_node/startup_script.sh
+++ dockerfiles/super_node/startup_script.sh
@@ -12,6 +12,8 @@ test $DATABASE_PORT
 test $DATABASE_USER
 test $DATABASE_PASSWORD
 test $IPFS_INIT
+test $IPFS_PATH
+test $VDB_COMMAND
 set +e
 
 # Export our database variables so that the IPFS Postgres plugin can use them
@@ -21,19 +23,34 @@ export IPFS_PGDATABASE=$DATABASE_NAME
 export IPFS_PGPORT=$DATABASE_PORT
 export IPFS_PGPASSWORD=$DATABASE_PASSWORD
 
-# If IPFS_INIT is true
-if [[ "$IPFS_INIT" = true ]] ; then
-    # initialize PG-IPFS
-    echo "Initializing Postgres-IPFS profile"
-    ./ipfs init --profile=postgresds
+# Construct the connection string for postgres
+VDB_PG_CONNECT=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable
+
+# Run the DB migrations
+echo "Connecting with: $VDB_PG_CONNECT"
+echo "Running database migrations"
+./goose -dir migrations/vulcanizedb postgres "$VDB_PG_CONNECT" up
+
+
+# If the db migrations ran without err
+if [[ $? -eq 0 ]]; then
+    # and IPFS_INIT is true
+    if [[ "$IPFS_INIT" = true ]] ; then
+        # initialize PG-IPFS
+        echo "Initializing Postgres-IPFS profile"
+        ./ipfs init --profile=postgresds
+    else
+        echo "IPFS profile already initialized, skipping initialization"
+    fi
 else
-    echo "IPFS profile already initialized, skipping initialization"
+    echo "Could not run migrations. Are the database details correct?"
+    exit 1
 fi
 
 # If IPFS initialization was successful
 if [[ $? -eq 0 ]]; then
     echo "Beginning the vulcanizedb super node process"
-    ./vulcanizedb superNode --config=config.toml 2>&1 | tee -a vulcanizedb.log &
+    ./vulcanizedb ${VDB_COMMAND} --config=config.toml 2>&1 | tee -a vulcanizedb.log &
 else
     echo "Could not initialize IPFS."
     exit 1
