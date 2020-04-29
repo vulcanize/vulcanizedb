a60404fcc14096e11e716c4374e5ece380ea1ba4
Ian Norden <iannordenn@gmail.com>
2020-04-28 19:33:59 -0500

    pg function to return valid diffs in a range back from a provided hash; update state_cache table

--- db/migrations/00034_create_state_cache_table.sql
+++ db/migrations/00034_create_state_cache_table.sql
@@ -1,7 +1,9 @@
 -- +goose Up
+-- the header_id is the id for the header the state_cache row belongs to, not necessarily the header_id of the referenced state_cid
 CREATE TABLE eth.state_cache (
   id               SERIAL PRIMARY KEY,
-  header_id        INTEGER NOT NULL REFERENCES eth.header_cids (id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  header_id        INTEGER NOT NULL REFERENCES eth.header_cids (id) ON DELETE CASCADE,
+  state_id         INTEGER NOT NULL REFERENCES eth.state_cids (id) ON DELETE CASCADE,
   state_path       BYTEA,
   mh_key           TEXT NOT NULL REFERENCES  public.blocks (key) ON DELETE CASCADE,
   UNIQUE (header_id, state_path)
--- db/migrations/00035_create_eth_header_validator_function.sql
+++ db/migrations/00035_create_eth_header_validator_function.sql
@@ -1,9 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
+-- Returns all of the header ids, in descending order, that are recursively validated from the provided hash
+-- includes the header id for the provided hash and the id found at height = depth
 CREATE OR REPLACE FUNCTION eth_valid_section(hash VARCHAR(66), depth INT)
         RETURNS TABLE (
-            id INT,
-            block_number BIGINT
+            id INT
 ) AS $$
   WITH RECURSIVE validator AS (
           SELECT id, parent_hash, block_number
@@ -17,9 +18,27 @@ CREATE OR REPLACE FUNCTION eth_valid_section(hash VARCHAR(66), depth INT)
              AND eth.header_cids.block_number = validator.block_number - 1
              AND eth.header_cids.block_number >= depth
   )
-  SELECT id, block_number FROM validator ORDER BY block_number DESC;
+  SELECT id FROM validator ORDER BY block_number DESC;
 $$ LANGUAGE SQL;
 -- +goose StatementEnd
 
+-- +goose StatementBegin
+-- Returns all of the latest state diffs (ids) for every path from height = n+1 to the height defined at the provided hash
+-- Use this to return all of the diffs to add on top of the state_cache at `n` to build the state_cache at `hash`
+CREATE OR REPLACE FUNCTION eth_valid_state_diffs(hash VARCHAR(66), n INT)
+        RETURNS TABLE (
+          state_id INT,
+          state_path BYTEA,
+          cid TEXT
+) AS $$
+  SELECT DISTINCT ON (state_path) state_cids.id, state_path, state_cids.cid
+  FROM eth.state_cids INNER JOIN eth.header_cids ON state_cids.header_id = header_cids.id
+  WHERE state_cids.header_id = ANY(SELECT * FROM eth_valid_section(hash, n+1))
+  ORDER BY state_path, block_number DESC;
+$$ LANGUAGE SQL;
+-- +goose StatementEnd
+
+
 -- +goose Down
 DROP FUNCTION eth_valid_section;
+DROP FUNCTION eth_valid_state_diffs;
